1fbc069160aa88fcaaa7835481c3a0d8498b1c8f
Danny Bauman <dannybauman@gmail.com>
2015-03-10 17:42:25 -0700

    Change canvas drawing so it clears and draws all nodes at once, so when nodes are removed from the data, they also are removed from the UI

--- example/index.js
+++ example/index.js
@@ -3,9 +3,8 @@ var Daffodil = require('../')
 document.addEventListener('click', onclick.bind(this))
 
 var daffodil = new Daffodil({broadcasterId: '0', k: 3})
-
-// draw the first node as the broadcaster
-drawNode(10, 10, '-1')
+var peerNodes = daffodil.root
+var peerCanvasCoords = {}
 
 function onclick (evt) {
   switch (evt.target.id) {
@@ -16,22 +15,46 @@ function onclick (evt) {
     case 'nodeMap':
       var newListenerId = generateNewId()
       daffodil.addListener(newListenerId)
-      drawNode.call(this, evt.pageX, evt.pageY, newListenerId)
+      peerCanvasCoords[newListenerId] = {id: newListenerId, x: evt.pageX, y: evt.pageY}
       break
   }
+  clearMap()
+  drawMap()
 }
 
 function generateNewId () {
   return Math.random().toString(36).substr(2, 4)
 }
 
-function drawNode (x, y, newListenerId) {
+function clearMap () {
+  var canvas = document.getElementById('nodeMap')
+  var ctx = canvas.getContext('2d')
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function drawMap () {
+  for (var peerNodeId in peerNodes.downstreamPeers) {
+    drawNode(peerNodes.downstreamPeers[peerNodeId])
+  }
+}
+
+function drawNode (peerNode) {
+  var peerCanvasCoord = peerCanvasCoords[peerNode.id]
+  if (!peerCanvasCoord) {
+    return
+  }
+
   var ctx = document.getElementById('nodeMap').getContext('2d')
 
   ctx.beginPath()
-  ctx.arc(x, y, 10, 0, 2 * Math.PI)
+  ctx.arc(peerCanvasCoord.x, peerCanvasCoord.y, 10, 0, 2 * Math.PI)
   ctx.stroke()
 
   ctx.font = '20px Georgia'
-  ctx.fillText(newListenerId, x - 15, y - 15)
+  ctx.fillText(peerCanvasCoord.id, peerCanvasCoord.x - 15, peerCanvasCoord.y - 15)
+
+  // recursively draw all the downstream peer nodes
+  for (var peerNodeId in peerNode.downstreamPeers) {
+    drawNode(peerNode.downstreamPeers[peerNodeId])
+  }
 }
